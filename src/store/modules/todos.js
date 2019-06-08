12a12f961473cd0509280577a9fdd71ada84705a
martyumlas <martyumlas@gmail.com>
2019-06-08 11:43:25 +0800

    fetching data from backend using actions and mutations

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -2,16 +2,7 @@ import axios from 'axios'
 
 const state = {
 
-    todos:[
-        {
-            id: 1,
-            title: 'Todo One'
-        },
-        {
-            id: 2,
-            title: 'Todo Two'
-        }
-    ]
+    todos:[]
 
 };
 
@@ -19,9 +10,19 @@ const getters = {
     allTodos: (state) => state.todos
 };
 
-const actions = {};
+const actions = {
+    async fetchTodos({commit}){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
-const mutations = {};
+        commit('setTodos' , response.data);
+    }
+};
+
+const mutations = {
+
+    setTodos: (state, todos) => (state.todos = todos)
+
+};
 
 export default{
     state,
